388b97133baeb9028c2fdd438b37dca3389942ed
Oumar Diarra <oumardiarra@Oumars-iMac.local>
2019-06-06 15:07:39 -0500

    Console log

--- oc-audit/src/server.js
+++ oc-audit/src/server.js
@@ -91,6 +91,7 @@ app.post('/admin/login', passport.authenticate('local', { failureRedirect: '/adm
 app.get('/admin/logout', adminController.logout);
 
 // Wrong urls Redirect
+app.get('/robots.txt', (req, res) => res.send('robots.txt'));
 app.get('*', (req, res) => res.redirect('/'));
 
 server = http.createServer(app);
